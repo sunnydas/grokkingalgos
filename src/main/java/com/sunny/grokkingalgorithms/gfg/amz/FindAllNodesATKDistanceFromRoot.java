79dc30cf16dede517d328a6c6824f814213679c1
Sunny Das <das.sunny@gmail.com>
2017-04-18 19:29:17 +0530

    Find all K nodes from root using BFS. Need to add more test cases to see if this is a valid solution.

---
+++ src/main/java/com/sunny/grokkingalgorithms/gfg/amz/FindAllNodesATKDistanceFromRoot.java
@@ -0,0 +1,78 @@
+package com.sunny.grokkingalgorithms.gfg.amz;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Created by sundas on 4/18/2017.
+ */
+public class FindAllNodesATKDistanceFromRoot {
+
+  //http://www.geeksforgeeks.org/print-nodes-at-k-distance-from-root/
+
+  /**
+   *
+   * @param root
+   * @param k
+   */
+  public static void printNodesAtKDIstanceFromRoot(Node root,int k){
+    Queue<Node> nodes = new LinkedList<>();
+    nodes.add(root);
+    Node level = new Node();
+    nodes.add(level);
+    int count = 0;
+    while(!nodes.isEmpty()){
+      Node node = nodes.poll();
+      if(node == level){
+        count++;
+        if(!nodes.isEmpty()) {
+          nodes.add(level);
+        }
+      }
+      else if(count == k){
+        Node cur = null;
+        System.out.println(node.data);
+        while(nodes.peek() != level && !nodes.isEmpty()){
+          cur = nodes.poll();
+          System.out.println(cur.data);
+        }
+        break;
+      }
+      if(node.left != null){
+        nodes.add(node.left);
+      }
+      if(node.right != null){
+        nodes.add(node.right);
+      }
+    }
+  }
+
+  /**
+   *
+   * @param args
+   */
+  public static void main(String[] args) {
+    Node root = new Node();
+    root.data = 1;
+    Node firstLeftOfRoot =  new Node();
+    firstLeftOfRoot.data = 2;
+    root.left = firstLeftOfRoot;
+    Node firstRightOfRoot = new Node();
+    firstRightOfRoot.data = 3;
+    root.right = firstRightOfRoot;
+    Node firstLeftOfTwo = new Node();
+    firstLeftOfTwo.data = 4;
+    firstLeftOfRoot.left = firstLeftOfTwo;
+    Node firstRighTOfTwo = new Node();
+    firstRighTOfTwo.data = 5;
+    firstLeftOfRoot.right = firstRighTOfTwo;
+    Node firstLeftOfThree = new Node();
+    firstLeftOfThree.data = 8;
+    firstRightOfRoot.left = firstLeftOfThree;
+    Node newNode = new Node();
+    newNode.data = 11;
+    firstLeftOfThree.right = newNode;
+    printNodesAtKDIstanceFromRoot(root,3);
+  }
+
+}
