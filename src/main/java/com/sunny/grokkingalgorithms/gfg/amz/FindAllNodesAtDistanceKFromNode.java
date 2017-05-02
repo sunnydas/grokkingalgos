f53db146f05b78ba215f2bbe1b47a7ebe8e8500e
Sunny Das <das.sunny@gmail.com>
2017-05-02 23:13:01 +0530

    Finding nodes k distance from target node (standard o(n) implementation).

--- src/main/java/com/sunny/grokkingalgorithms/gfg/amz/FindAllNodesAtDistanceKFromNode.java
+++ src/main/java/com/sunny/grokkingalgorithms/gfg/amz/FindAllNodesAtDistanceKFromNode.java
@@ -24,6 +24,70 @@ public class FindAllNodesAtDistanceKFromNode {
 
   /**
    *
+   * @param current
+   * @param k
+   */
+  public static void printNodeAtKDistanceFromTargetDFS(Node current,int k){
+    //redundant
+    if(current == null && k < 0){
+      return;
+    }
+    if(k == 0){
+      System.out.println(current.data);
+    }
+    if(current.left != null){
+      printNodeAtKDistanceFromTargetDFS(current.left,k-1);
+    }
+    if(current.right != null){
+      printNodeAtKDistanceFromTargetDFS(current.right,k-1);
+    }
+  }
+
+  /**
+   * Standard algorithm
+   *
+   *
+   */
+  public static int printNodesAtDistanceK(Node root,Node target,int k){
+    if(root == null){
+      return -1;
+    }
+    /*
+    Found target node
+     */
+    if(root == target) {
+      printNodeAtKDistanceFromTargetDFS(root,k);
+      return 0;
+    }
+    // iof target node is on left of root
+    int dl = printNodesAtDistanceK(root.left,target,k);
+    if(dl != -1){
+      // target node is on left
+      if(dl + 1 == k){
+        System.out.println(root.data);
+      }
+      else if(root.right != null){
+        printNodeAtKDistanceFromTargetDFS(root.right,k - dl - 2);
+      }
+      return dl + 1;
+    }
+    int dr = printNodesAtDistanceK(root.right,target,k);
+    if(dr != -1){
+      // target node is on left
+      if(dr + 1 == k){
+        System.out.println(root.data);
+      }
+      else if(root.left != null){
+        printNodeAtKDistanceFromTargetDFS(root.left,k - dr - 2);
+      }
+      return dr + 1;
+    }
+    return -1;
+  }
+
+
+  /**
+   *
    * @param child
    * @param parents
    * @param k
@@ -124,7 +188,7 @@ public class FindAllNodesAtDistanceKFromNode {
     newNode.data = 11;
     firstLeftOfThree.right = newNode;
     System.out.println("----------------------------------");
-    printAllNodesAtDistanceKfromNode(root, firstLeftOfRoot, 3);
+    //printAllNodesAtDistanceKfromNode(root, firstLeftOfRoot, 3);
     root = new Node(20);
     root.left = new Node(8);
     root.right = new Node(22);
@@ -135,6 +199,8 @@ public class FindAllNodesAtDistanceKFromNode {
     Node target = root.left.right;
     System.out.println("----------------------------------");
     printAllNodesAtDistanceKfromNode(root, target, 2);
+    System.out.println("-----------standard-------");
+    printNodesAtDistanceK(root, target, 2);
     target = root.left.right.right;
     System.out.println("----------------------------------");
     printAllNodesAtDistanceKfromNode(root, target, 3);
@@ -144,6 +210,7 @@ public class FindAllNodesAtDistanceKFromNode {
     System.out.println("----------------------------------");
     target = root.left.right;
     printAllNodesAtDistanceKfromNode(root, target, 1);
+    System.out.println("----------------------------------");
     printAllNodesAtDistanceKfromNode(root, target, 2);
     System.out.println("----------------------------------");
     target = root.left.right;
@@ -159,6 +226,35 @@ public class FindAllNodesAtDistanceKFromNode {
     System.out.println("----------------------------------");
     target = root.left.right.right;
     printAllNodesAtDistanceKfromNode(root, target, 2);
+    // standard
+    System.out.println("----------------------------------Standard-----");
+    target = root.left.right;
+    printNodesAtDistanceK(root, target, 2);
+    target = root.left.right.right;
+    System.out.println("----------------------------------");
+    printNodesAtDistanceK(root, target, 3);
+    target = root.left;
+    System.out.println("----------------------------------");
+    printNodesAtDistanceK(root, target, 2);
+    System.out.println("----------------------------------");
+    target = root.left.right;
+    printNodesAtDistanceK(root, target, 1);
+    System.out.println("------------------------------------");
+    printNodesAtDistanceK(root, target, 2);
+    System.out.println("----------------------------------");
+    target = root.left.right;
+    printNodesAtDistanceK(root, target, 3);
+    System.out.println("----------------------------------");
+    printNodesAtDistanceK(root, target, 2);
+    System.out.println("----------------------------------");
+    target = root.left.right;
+    printNodesAtDistanceK(root, target, 4);
+    System.out.println("----------------------------------");
+    target = root.left.right.right;
+    printNodesAtDistanceK(root, target, 1);
+    System.out.println("----------------------------------");
+    target = root.left.right.right;
+    printNodesAtDistanceK(root, target, 2);
   }
 
 
