e9025b8b4e3ebcacad210695918de16eed60d295
Sunny Das <das.sunny@gmail.com>
2017-06-03 13:00:47 +0530

    Recursive version of binary search.

--- src/main/java/com/sunny/grokkingalgorithms/introtoalg/BinarySearch.java
+++ src/main/java/com/sunny/grokkingalgorithms/introtoalg/BinarySearch.java
@@ -9,6 +9,32 @@ public class BinarySearch {
   /**
    *
    * @param input
+   * @param start
+   * @param end
+   * @return
+   */
+  public static int binarySearchRecursive(int[] input,int start,int end,int needle){
+    if(start > end){
+      return -1;
+    }
+    int mid = (start + end)/2;
+    //Found needle in haystack
+    if(needle == input[mid]){
+      return mid;
+    }
+    if(needle > input[mid]){
+      // search right recursively
+      return binarySearchRecursive(input,mid+1,end,needle);
+    }
+    else{
+      //search left recursively
+      return binarySearchRecursive(input,start,mid - 1,needle);
+    }
+  }
+
+  /**
+   *
+   * @param input
    * @param needle
    * @return
    */
@@ -44,6 +70,9 @@ public class BinarySearch {
   public static void main(String[] args) {
     int[] input = {1,3,4,17,17,25,45,46,100};
     System.out.println(binarySearch(input,900));
+    System.out.println(binarySearch(input,46));
+    System.out.println(binarySearchRecursive(input, 0, input.length - 1, 46));
+    System.out.println(binarySearchRecursive(input, 0, input.length - 1, 900));
   }
 
 }
