ee42a20d85dacb7ecc5a3b79821d2760cc114430
Sunny Das <das.sunny@gmail.com>
2017-11-12 22:26:47 +0530

    DP based solution for contigous subarray problem.

--- src/main/java/com/sunny/grokkingalgorithms/gfg/mustdo/ContinousSubArray.java
+++ src/main/java/com/sunny/grokkingalgorithms/gfg/mustdo/ContinousSubArray.java
@@ -60,6 +60,104 @@ public class ContinousSubArray {
     }
   }
 
+
+   /**
+   * @param input
+   * @param totalSum
+   */
+  public static void findContinousSubArrayv2(int[]  input, int totalSum){
+    int startIndex = -1;
+    int endIndex = -1;
+    /*
+    We will try to use DP
+     */
+    // initialize matrix for calculating sums
+    int[][] partialSums = new int[input.length][input.length];
+    /*
+    Example on how it will work
+    suppose we have array = [1,2,3] and we have to find sum 5
+    The matrix will look like:
+         1 3 6
+         x 2 5
+         x x 3
+        the startIndex = 1 , end Index = 2
+     */
+    partialSums = populatePartialSumMatrix(input,partialSums);
+    //printMatrix(partialSums);
+    /*
+    Now we just have to search the sum in the matrix
+     */
+    boolean found = false;
+    int rowTracker = 0;
+    int colTracker = 0;
+    for(int i = rowTracker; i < partialSums.length ; i++){
+      for(int j = colTracker; j < partialSums.length; j++){
+        if(partialSums[i][j] == totalSum){
+          startIndex = i;
+          endIndex = j;
+          found = true;
+          break;
+        }
+      }
+      if(found){
+        break;
+      }
+      rowTracker++;
+      colTracker++;
+    }
+    if(startIndex >= 0 && endIndex >= 0) {
+      System.out.println((startIndex + 1) + " " + (endIndex+1));
+    }
+    else{
+      System.out.println(-1);
+    }
+  }
+
+  /**
+   *
+   * @param matrix
+   */
+  public static void printMatrix(int[][] matrix){
+    for(int i = 0 ; i < matrix.length ; i++){
+      for(int j = 0 ; j < matrix.length ; j++){
+        System.out.print(matrix[i][j]);
+        System.out.print(" ");
+      }
+      System.out.println();
+    }
+  }
+
+  /**
+   *
+   * @param input
+   * @param partialSumMatrix
+   * @return
+   */
+  public static int[][] populatePartialSumMatrix(int[]  input,int[][] partialSumMatrix){
+    int offset = 0;
+    int rowTracker = 0;
+    int colTracker = 0;
+    int runningSum = 0;
+    for(int i = rowTracker; i < partialSumMatrix.length ; i++){
+      for(int j = colTracker; j < partialSumMatrix[i].length ; j++){
+        int placeHolderVal = 0;
+        if((i - 1) >= 0){
+          placeHolderVal = partialSumMatrix[i - 1][j] - offset;
+        }
+        else{
+          runningSum += input[j];
+          placeHolderVal = runningSum ;
+        }
+        partialSumMatrix[i][j] = placeHolderVal;
+      }
+      offset = partialSumMatrix[rowTracker][colTracker];
+      rowTracker++;
+      colTracker++;
+    }
+    return partialSumMatrix;
+  }
+
+
   /**
    *
    * @param args
@@ -74,7 +172,8 @@ public class ContinousSubArray {
       for(int j = 0 ; j < sizeOfArray ; j++){
         input[j] = scanner.nextInt();
       }
-      findContinousSubArray(input,totalSum);
+      //findContinousSubArray(input,totalSum);
+      findContinousSubArrayv2(input, totalSum);
     }
   }
 
