4bc771e9b9a0b3a9e6dd61e9d8b593a2ad40a6a4
Sunny Das <das.sunny@gmail.com>
2018-03-12 16:55:28 +0530

    Rotate a matrix by 90 degrees clockwise in place.

--- src/main/java/com/sunny/grokkingalgorithms/ctci/c1/MatrixRotation.java
+++ src/main/java/com/sunny/grokkingalgorithms/ctci/c1/MatrixRotation.java
@@ -43,6 +43,67 @@ public class MatrixRotation {
   }
 
 
+  /**
+   *
+   * @param matrix
+   * @return
+   */
+  public static int[][] rotateBy90InPlace(int[][] matrix){
+    int length = matrix.length - 1;
+    /*
+    In place rotation works for square matrix
+     */
+    /*
+    1 2 3
+    4 5 6
+    7 8 9
+    layer 1
+    1,3 ,9 and 7 are the important co ordinates
+    7 2 1
+    4 5 6
+    9 8 3
+    step 2 for layer 1
+    7 4 1
+    8 5 2
+    9 6 3
+    layer 1 has heen rotated
+    layer 2 is just 5 and there is no need for rotation
+     */
+    /*
+    Outer loop represents layer so in each later two rows are considered
+     */
+    for(int i = 0 ; i <= (length/2) ; i++){
+      /*
+      We do not eed to worry about previous layer since it is already rotaed
+       */
+      /*
+      For each cell in layer
+       */
+      for(int j = i; j < (length - i) ; j++){
+        /*
+        1 2
+        3 4
+        i=0, j = 0
+        p1 = 1
+        p2 = 2 (0,length - 1)
+        p3 = 4
+        p4 = 3
+         */
+        int p1 = matrix[i][j];
+        int p2 = matrix[j][length - i];
+        int p3 = matrix[length - i][length - j];
+        int p4 = matrix[length- j][i];
+        //Swap
+        matrix[i][j] = p4;
+        matrix[j][length] = p1;
+        matrix[length - i][length - j] = p2;
+        matrix[length - j][i] = p3;
+      }
+    }
+    return matrix;
+  }
+
+
   public static void printMatrix(int[][] input){
     for(int i = 0 ; i < input.length ; i++){
       for(int j = 0 ; j < input[i].length ; j++){
@@ -78,6 +139,31 @@ public class MatrixRotation {
     };
     printMatrix(matrix);
     rotated = rotateBy90(matrix);
+    System.out.println("After rotation ....");
+    if(rotated != null){
+      printMatrix(rotated);
+    }
+    System.out.println("In place");
+    matrix = new int[][]{
+        {1,2,3},
+        {4,5,6},
+        {7,8,9}
+    };
+    printMatrix(matrix);
+    rotated = rotateBy90InPlace(matrix);
+    System.out.println("After rotation ....");
+    if(rotated != null){
+      printMatrix(rotated);
+    }
+    System.out.println("next");
+    matrix = new int[][]{
+        {1,2,3,4},
+        {5,6,7,8},
+        {9,10,11,12},
+        {13,14,15,16}
+    };
+    printMatrix(matrix);
+    rotated = rotateBy90InPlace(matrix);
     System.out.println("After rotation ....");
     if(rotated != null){
       printMatrix(rotated);
