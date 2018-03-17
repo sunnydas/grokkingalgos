452ef03fad73d988c0de7b6ffe755fe9acee8772
Sunny Das <das.sunny@gmail.com>
2018-03-17 22:19:19 +0530

    Implementing popAt(stackIndex). This requires shuffling of data.

--- src/main/java/com/sunny/grokkingalgorithms/ctci/c3/SetOfStacks.java
+++ src/main/java/com/sunny/grokkingalgorithms/ctci/c3/SetOfStacks.java
@@ -83,16 +83,64 @@ public class SetOfStacks {
   }
 
   /**
+   * Pops an element from a specific stack
+   *
+   * @param stackIndex
+   * @return
+   */
+  public int popAt(int stackIndex){
+    /*
+       So if we pop an element from a stack , if there are stacks after this stack which are filled we may have to
+       fill it to preserve stack ordering. We dont have to worry about stacks before this stack, only stacks that are
+        after this. Essentially we have created a hold in this stack and that needs to be filled out by shuffling
+        elements from other stacks that follow this stack.
+     */
+    int elem = Integer.MAX_VALUE;
+    if(stackIndex < stacks.length && stacks[stackIndex] != null && !stacks[stackIndex].isEmpty()){
+      elem = stacks[stackIndex].pop();
+      /*
+      Now if we have stacks fater this which are full, we have essentially created the hole. SO we need to shift one
+      element from next stack to this stack and repeat until we have done this for all stacks
+       */
+      int stackPointer = stackIndex;
+      /*
+      If there are next stacks which have elements in them
+       */
+      while((stackPointer+1) < stacks.length
+          && stacks[stackPointer+1] != null && !stacks[stackPointer+1].isEmpty()){
+        Stack<Integer> auxillaryStack = new Stack<>();
+        //Copy next stack elements to auxillary stack
+        while(!stacks[stackPointer+1].isEmpty()){
+          auxillaryStack.push(stacks[stackPointer+1].pop());
+        }
+        // push last element of next stack to current stack
+        if(!auxillaryStack.isEmpty()){
+          stacks[stackPointer].push(auxillaryStack.pop());
+        }
+        //Copy back the auxillary elements to next stack
+        while(!auxillaryStack.isEmpty()){
+          stacks[stackPointer+1].push(auxillaryStack.pop());
+        }
+        //Repeat
+        stackPointer++;
+      }
+    }
+    return elem;
+  }
+
+  /**
    *
    * @param args
    */
   public static void main(String[] args) {
     int[] input = new int[]{1,2,3,4,5,6,0,-1,-200,100,-100,10};
-    SetOfStacks setOfStacks = new SetOfStacks(4,3);
+    SetOfStacks setOfStacks = new SetOfStacks(3,4);
     for(int i = 0 ; i < input.length ; i++){
       setOfStacks.push(input[i]);
     }
-
+    System.out.println(setOfStacks.popAt(1));
+    setOfStacks.push(120);
+    System.out.println("Bulk pop");
     for(int i = 0 ; i < input.length ; i++){
       System.out.println(setOfStacks.pop());
     }
