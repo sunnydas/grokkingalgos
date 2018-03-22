bdea5670a6420e64b115cf0a01bcc770a06ad9cf
Sunny Das <das.sunny@gmail.com>
2018-03-23 01:13:30 +0530

    Additional function for 1 ,2 steps.

--- src/main/java/com/sunny/grokkingalgorithms/ctci/c9plusdp/FindStepsRequired.java
+++ src/main/java/com/sunny/grokkingalgorithms/ctci/c9plusdp/FindStepsRequired.java
@@ -12,6 +12,16 @@ public class FindStepsRequired {
 
   public static int possibleWays = 0;
 
+   /*
+   Approach:
+
+Say child has to take n steps.
+At every step the child has 3 options, to take 1 step, 2 step or 3 steps.
+So if child take 1 step then find the number of ways to complete n-1 steps +1.
+Similarly if child take 2 steps then find the number of ways to complete n-2 steps +1.
+If child take 3 step then find the number of ways to complete n-3 steps +1.
+So total number of ways to complete n steps = No of ways to complete (n-1)steps + No of ways to complete (n-2)steps + No of ways to complete (n-3)steps +1.
+    */
 
   /**
    *
@@ -54,6 +64,24 @@ public class FindStepsRequired {
     findPossibleSteps(n-3);
   }
 
+  /**
+   *
+   * @param n
+   * @return
+   */
+  public static void findPossibleStepsTwo(int n){
+    if(n == 0){
+      possibleWays++;
+      return;
+    }
+    else if(n < 0){
+      return;
+    }
+    findPossibleSteps(n-1);
+    findPossibleSteps(n-2);
+    //findPossibleSteps(n-3);
+  }
+
 
   /**
    *
@@ -69,11 +97,14 @@ public class FindStepsRequired {
     long start = System.currentTimeMillis();
     findPossibleSteps(30);
     System.out.println(possibleWays);
-    System.out.println(" Time taken = " + ((System.currentTimeMillis() - start))/100 + " seconds");
+    System.out.println(" Time taken = " + ((System.currentTimeMillis() - start)) / 100 + " seconds");
     System.out.println(findPossibleStepsDP(4, new int[5]));
     System.out.println(findPossibleStepsDP(3,new int[4]));
     start = System.currentTimeMillis();
     System.out.println(findPossibleStepsDP(30,new int[31]));
     System.out.println(" Time taken = " + ((System.currentTimeMillis() - start))/100 + " seconds");
+    possibleWays = 0;
+    findPossibleStepsTwo(2);
+    System.out.println(possibleWays);
   }
 }
