1ac7f94e65324387a13d033a5147486fd06de71d
Sunny Das <das.sunny@gmail.com>
2018-03-30 00:34:22 +0530

    Alternative approach using the same array.

--- src/main/java/com/sunny/grokkingalgorithms/gfg/it/FindMinMissingNumber.java
+++ src/main/java/com/sunny/grokkingalgorithms/gfg/it/FindMinMissingNumber.java
@@ -1,5 +1,9 @@
 package com.sunny.grokkingalgorithms.gfg.it;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * Created by sundas on 3/29/2018.
  */
@@ -17,6 +21,77 @@ public class FindMinMissingNumber {
  Input: {1, 1, 0, -1, -2}
  Output: 2
    */
+
+
+  /**
+   *
+   * @param input
+   * @return
+   */
+  public static int findMissingMinimumPostiveOptimized(int[] input){
+    /*
+    The logic is that we use something similar to counting sort.
+    Let us understand by an example:
+    {0, 2, 3, -7, 6, 8, 1, -10, 15 }
+    1.) Get rid of negatives
+       {2,3,6.8,1,15}
+     indexes =    0,1,2,3,4,5
+    2.) find numbers
+     iter 1: 2,-3 ,6,8,1,15
+     iter 2: 2,-3,-6,8,1,15
+     iter 3; 2,-3,-6,8,1,-15
+     iter 4: 2,-3,-6,8,1,-15
+     iter 5: -2,-3,-6,8,1,-15
+     iter 6: -2,-3,-6,8,1,-15
+      First positive number at index 3 so missing number is 4.
+     */
+    int start = seggregate(input);
+    int[] shifted = new int[input.length - start];
+    int j = 0;
+    for(int i = start; i < input.length ; i++){
+      shifted[j] = input[i];
+      j++;
+    }
+    return findMissingPositiveAfterSeggregation(shifted,start);
+  }
+
+  /**
+   *
+   * @param input
+   */
+  public static int seggregate(int[] input){
+    int j = 0;
+    for(int i = 0 ; i < input.length ; i++){
+      if(input[i] <= 0){
+        int temp = input[i];
+        input[i] = input[j];
+        input[j] = temp;
+        j++;
+      }
+    }
+    return j;
+  }
+
+
+  /**
+   *
+   * @param input
+   * @return
+   */
+  public static int findMissingPositiveAfterSeggregation(int[] input,int offset){
+    for(int i = 0 ; i < input.length ; i++){
+      if(Math.abs(input[i])-1 >= 0
+          && Math.abs(input[i]) - 1< input.length && input[Math.abs(input[i]) - 1] > 0){
+        input[Math.abs(input[i]) - 1] = -input[Math.abs(input[i]) - 1];
+      }
+    }
+    for(int i = 0 ; i < input.length ; i++){
+      if(input[i] > 0){
+        return i+1;
+      }
+    }
+    return input.length + 1;
+  }
 
   /**
    *
@@ -80,7 +155,7 @@ public class FindMinMissingNumber {
         }
       }
     }
-    else if(maxPositive - minPositive == 1){
+    else if((maxPositive - minPositive) == 1){
       min = maxPositive + 1;
     }
     return min;
@@ -93,12 +168,20 @@ public class FindMinMissingNumber {
   public static void main(String[] args) {
     int[] input = {-10,7,-5,4,2};
     System.out.println(findMimimumMissingPositiveNumber(input));
+    System.out.println("optimized");
+    System.out.println(findMissingMinimumPostiveOptimized(input));
     input = new int[]{0, 2, 3, -7, 6, 8, 1, -10, 15};
     System.out.println(findMimimumMissingPositiveNumber(input));
+    System.out.println("optimized");
+    System.out.println(findMissingMinimumPostiveOptimized(input));
     input = new int[]{2, 3, 7, 6, 8, -1, -10, 15};
     System.out.println(findMimimumMissingPositiveNumber(input));
+    System.out.println("optimized");
+    System.out.println(findMissingMinimumPostiveOptimized(input));
     input = new int[]{1, 1, 0, -1, -2};
     System.out.println(findMimimumMissingPositiveNumber(input));
+    System.out.println("optimized");
+    System.out.println(findMissingMinimumPostiveOptimized(input));
   }
 
 
