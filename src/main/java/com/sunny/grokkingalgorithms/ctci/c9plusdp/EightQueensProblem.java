853077ff1e075dce38d72d1dbb5a1dbc87d0ccdc
Sunny Das <das.sunny@gmail.com>
2018-04-06 01:59:05 +0530

    N queens problem using back tracking , needed some help here.

--- src/main/java/com/sunny/grokkingalgorithms/ctci/c9plusdp/EightQueensProblem.java
+++ src/main/java/com/sunny/grokkingalgorithms/ctci/c9plusdp/EightQueensProblem.java
@@ -1,8 +1,26 @@
 package com.sunny.grokkingalgorithms.ctci.c9plusdp;
 
+import com.sunny.grokkingalgorithms.ctci.c1.MatrixRotation;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by sundas on 4/4/2018.
  */
+class ChessPoint{
+  int i;
+  int j;
+
+  @Override
+  public String toString() {
+    return "ChessPoint{" +
+        "i=" + i +
+        ", j=" + j +
+        '}';
+  }
+}
 public class EightQueensProblem {
 
   /*
@@ -12,9 +30,75 @@ public class EightQueensProblem {
 
   /**
    *
+   * @param n
+   */
+  public static void solveNQueens(int n){
+    /*
+    The position array gets overwritten so we do not need to clear explicitly
+     */
+    ChessPoint[] chessPoints = new ChessPoint[n];
+    boolean solutionExists = solveNQueensUtil(n,0,chessPoints);
+    if(solutionExists){
+      Arrays.stream(chessPoints).forEach(position -> System.out.println(position.i + " " + position.j));
+    }
+  }
+
+  /**
+   *
+   * @param n
+   * @param row
+   * @param points
+   * @return
+   */
+  public static boolean solveNQueensUtil(int n,int row,ChessPoint[] points){
+    if(n == row) {
+      return true;
+    }
+    for(int i = 0 ; i < n ; i++){
+      ChessPoint point = new ChessPoint();
+      point.i = row;
+      point.j = i;
+      if(isSafe(points,point)){
+        points[row] = point;
+        boolean solutionExists = solveNQueensUtil(n,row+1,points);
+        if(solutionExists){
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  /**
+   *
+   * @param points
+   * @param current
+   * @return
+   */
+  public static boolean isSafe(ChessPoint[] points,ChessPoint current){
+    boolean safe = true;
+    /*
+    Only check before the current row
+     */
+    /*
+    Important formula
+     */
+    for(int q = 0; q < current.i ; q++){
+      if(points[q].j == current.j || points[q].i - points[q].j == current.i - current.j || points[q].i + points[q].j
+          == current.i + current.j){
+        safe = false;
+        break;
+      }
+    }
+    return safe;
+  }
+
+
+  /**
+   *
    * @param args
    */
   public static void main(String[] args) {
-
+    solveNQueens(8);
   }
 }
