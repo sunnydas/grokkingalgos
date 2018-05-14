b1e3537e20262b219d6feb0427a662a94a320339
Sunny Das <das.sunny@gmail.com>
2018-05-14 10:23:00 +0530

    Zombie cluster brute force implementation.

--- src/main/java/com/sunny/grokkingalgorithms/gfg/rlck/ZombieCluster.java
+++ src/main/java/com/sunny/grokkingalgorithms/gfg/rlck/ZombieCluster.java
@@ -1,6 +1,7 @@
 package com.sunny.grokkingalgorithms.gfg.rlck;
 
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 /**
@@ -9,23 +10,67 @@ import java.util.Set;
 public class ZombieCluster {
 
 
+  /**
+   *
+   * @param zombies
+   * @return
+   */
   public static int zombieCluster(String[] zombies){
     int zombieClusters = 0;
     Set<Set<Integer>> consideredIndexes = new HashSet<>();
-    populateZomibieClusters(consideredIndexes,zombies,0);
+    for(int i = 0 ; i < zombies.length ; i++) {
+      Set<Integer> currentCluster = new HashSet<>();
+      populateZombieCluster(currentCluster, zombies, i,consideredIndexes);
+      if(currentCluster.size() > 0){
+        consideredIndexes.add(currentCluster);
+      }
+    }
+    System.out.println(consideredIndexes);
+    zombieClusters = consideredIndexes.size();
     return zombieClusters;
   }
 
-  public static void populateZomibieClusters(Set<Set<Integer>> consideredIndexes,String[] zombies,int index){
+  /**
+   *
+   * @param currentCluster
+   * @param zombies
+   * @param index
+   * @param consideredIndexes
+   */
+  public static void populateZombieCluster(Set<Integer> currentCluster,String[] zombies,int index
+  ,Set<Set<Integer>> consideredIndexes){
     if(index > zombies.length){
       return;
     }
-    
+    String currentZombie = zombies[index];
+    if(currentZombie.charAt(index) == '1' && !alreadyConsideredIndex(consideredIndexes,index)){
+      currentCluster.add(index);
+    }
+    for(int i = index+1; i < currentZombie.length() ;i++){
+      if(currentZombie.charAt(i) == '1') {
+        populateZombieCluster(currentCluster, zombies, i, consideredIndexes);
+      }
+    }
   }
 
 
+  /**
+   *
+   * @param consideredIndexes
+   * @param index
+   * @return
+   */
   public static boolean alreadyConsideredIndex(Set<Set<Integer>> consideredIndexes,int index){
-    return false;
+    boolean considered = false;
+    Iterator<Set<Integer>> iterator = consideredIndexes.iterator();
+    while(iterator.hasNext()){
+      Set<Integer> cluster = iterator.next();
+      if(cluster.contains(index)){
+        considered = true;
+        break;
+      }
+    }
+    return considered;
   }
 
 
@@ -38,6 +83,23 @@ public class ZombieCluster {
           "0001"
       };
     System.out.println(zombieCluster(zombies));
+    System.out.println("#########");
+    zombies = new String[]{
+        "1000",
+        "010",
+        "0010",
+        "0001"
+    };
+    System.out.println(zombieCluster(zombies));
+    System.out.println("#####################");
+    zombies = new String[]{
+        "1001",
+        "010",
+        "0010",
+        "1001"
+    };
+    System.out.println(zombieCluster(zombies));
+    System.out.println("#######################");
   }
 
 }
