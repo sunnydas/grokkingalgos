aa061c48323878516e5fe3a93516128ed5954db1
Sunny Das <das.sunny@gmail.com>
2018-06-12 14:51:46 +0530

    Fibinacci using memoization.

--- src/main/java/com/sunny/grokkingalgorithms/fasttrack/dynamicprogrammingandrecursion/FibonacciNumbers.java
+++ src/main/java/com/sunny/grokkingalgorithms/fasttrack/dynamicprogrammingandrecursion/FibonacciNumbers.java
@@ -1,5 +1,8 @@
 package com.sunny.grokkingalgorithms.fasttrack.dynamicprogrammingandrecursion;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by sundas on 6/12/2018.
  */
@@ -8,6 +11,20 @@ public class FibonacciNumbers {
    /*
    Fibonacci series
     */
+
+  static Map<Integer,Long> memoizationCache = new HashMap<>();
+
+  public static long fibonacciUsingMemoization(int n){
+    if(n == 1 || n <= 0){
+      return 1;
+    }
+    if(memoizationCache.containsKey(n)){
+      return memoizationCache.get(n);
+    }
+    long fibN = fibonacciUsingMemoization(n - 1) + fibonacciUsingMemoization(n - 2);
+    memoizationCache.put(n,fibN);
+    return fibN;
+  }
 
   /**
    *
@@ -29,6 +46,15 @@ public class FibonacciNumbers {
     System.out.println(fibonacci(5));
     System.out.println(fibonacci(6));
     System.out.println(fibonacci(7));
+    long begin = System.currentTimeMillis();
+    System.out.println(fibonacci(50));
+    System.out.println("time taken = " + (System.currentTimeMillis() - begin));
+    System.out.println(fibonacciUsingMemoization(5));
+    System.out.println(fibonacciUsingMemoization(6));
+    System.out.println(fibonacciUsingMemoization(7));
+    begin = System.currentTimeMillis();
+    System.out.println(fibonacciUsingMemoization(50));
+    System.out.println("time taken = " + (System.currentTimeMillis() - begin));
   }
 
 }
