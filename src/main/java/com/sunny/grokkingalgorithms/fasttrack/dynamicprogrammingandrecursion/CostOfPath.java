52099ebc8a540a0df72d21e3948df880ff84793e
Sunny Das <das.sunny@gmail.com>
2018-06-13 12:04:15 +0530

    Find minimum cost from top left cell to bottom right cell ,using bottom up.

--- src/main/java/com/sunny/grokkingalgorithms/fasttrack/dynamicprogrammingandrecursion/CostOfPath.java
+++ src/main/java/com/sunny/grokkingalgorithms/fasttrack/dynamicprogrammingandrecursion/CostOfPath.java
@@ -14,6 +14,31 @@ public class CostOfPath {
 
   static  int minCost = Integer.MAX_VALUE;
 
+  public static int findMinCostBottomUp(int[][] costMatrix,
+                                        int i,
+                                        int j,
+                                        int curCost,
+                                        String path){
+    curCost += costMatrix[i][j];
+    path += costMatrix[i][j] + "-";
+    if(i == 0 && j == 0){
+      System.out.println(path);
+      /*
+      Found cell
+       */
+      return curCost;
+    }
+    int x = Integer.MAX_VALUE;
+    int y = Integer.MAX_VALUE;
+    if(i - 1 >= 0){
+      x = findMinCostBottomUp(costMatrix, i - 1, j, curCost, path);
+    }
+    if(j - 1 >= 0){
+      y = findMinCostBottomUp(costMatrix, i, j - 1, curCost, path);
+    }
+    return Math.min(x,y);
+  }
+
   /**
    *
    * @param costMatrix
@@ -66,12 +91,12 @@ public class CostOfPath {
      */
     if(i == costMatrix.length - 1
         && j == costMatrix[i].length - 1){
+      System.out.println("Path: " + path);
       /*
       Reached end
        */
       if(curCost < minCost){
         System.out.println("Cost: " + curCost);
-        System.out.println("Path: " + path);
         minCost = curCost;
       }
       return;
@@ -98,6 +123,11 @@ public class CostOfPath {
     System.out.println(minCost);
     System.out.println("#########");
     System.out.println(findMinCost(costMatrix,0,0,0));
+    System.out.println("#########");
+    System.out.println(findMinCostBottomUp(costMatrix,
+        costMatrix.length - 1,
+        costMatrix[costMatrix.length - 1].length - 1,
+        0,""));
   }
 
 }
