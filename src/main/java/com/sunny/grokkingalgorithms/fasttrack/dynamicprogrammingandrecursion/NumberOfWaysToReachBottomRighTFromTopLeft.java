2eac2da1fa71318803ab88dba00b2eaf85e9880e
Sunny Das <das.sunny@gmail.com>
2018-06-13 22:12:39 +0530

    Count number if ways to reach bottom right cell from top left cell. DP solution O(mxn).

--- src/main/java/com/sunny/grokkingalgorithms/fasttrack/dynamicprogrammingandrecursion/NumberOfWaysToReachBottomRighTFromTopLeft.java
+++ src/main/java/com/sunny/grokkingalgorithms/fasttrack/dynamicprogrammingandrecursion/NumberOfWaysToReachBottomRighTFromTopLeft.java
@@ -10,6 +10,28 @@ public class NumberOfWaysToReachBottomRighTFromTopLeft {
   only right or down.
    */
 
+  public static int countNumberOfWaysDP(int m,int n){
+    int[][] dp = new int[m][n];
+    for(int j = 1; j < dp[0].length ; j++){
+      dp[0][j] = 1;// there is only way to reach this cell
+    }
+    for(int i = 1; i < dp.length ; i++){
+      dp[i][0] = 1;// there is only way to reach this cell
+    }
+    for(int i = 1; i < dp.length ; i++){
+      for(int j = 1; j < dp[i].length ; j++){
+        dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+      }
+    }
+    for(int i = 0 ; i < dp.length ; i++){
+      for(int j = 0 ; j < dp[i].length ; j++){
+        System.out.print(dp[i][j] + " ");
+      }
+      System.out.println();
+    }
+    return dp[m-1][n-1];
+  }
+
   /**
    *
    * @param m
@@ -32,6 +54,9 @@ public class NumberOfWaysToReachBottomRighTFromTopLeft {
   public static void main(String[] args) {
     System.out.println(countNumberOfWays(2,2));
     System.out.println(countNumberOfWays(3,4));
+    System.out.println("######################");
+    System.out.println(countNumberOfWaysDP(2,2));
+    System.out.println(countNumberOfWaysDP(3,4));
   }
 
 }
