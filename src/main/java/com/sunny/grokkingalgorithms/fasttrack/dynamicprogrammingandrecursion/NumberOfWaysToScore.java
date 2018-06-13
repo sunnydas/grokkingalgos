9e65c8106e412631395c677a2cea28f45852b1bf
Sunny Das <das.sunny@gmail.com>
2018-06-13 14:29:23 +0530

    Count number of ways to reach a score of n using dp.

--- src/main/java/com/sunny/grokkingalgorithms/fasttrack/dynamicprogrammingandrecursion/NumberOfWaysToScore.java
+++ src/main/java/com/sunny/grokkingalgorithms/fasttrack/dynamicprogrammingandrecursion/NumberOfWaysToScore.java
@@ -15,6 +15,28 @@ public class NumberOfWaysToScore {
   /**
    *
    * @param n
+   * @return
+   */
+  public static int countNumberOfWaysToReachScoreNDP(int n){
+    int[] numWays = new int[n+1];
+    numWays[0] = 1;
+    for(int i = 1 ; i <= n ; i++){
+      if(i - 3 >= 0){
+        numWays[i] += numWays[i - 3];
+      }
+      if(i - 5 >= 0){
+        numWays[i] += numWays[i - 5];
+      }
+      if(i - 10 >= 0){
+        numWays[i] += numWays[i - 10];
+      }
+    }
+    return numWays[n];
+  }
+
+  /**
+   *
+   * @param n
    * @param scoreTrace
    */
   public static void countNumberOfWaysToReachScoreN(int n,String scoreTrace){
@@ -39,6 +61,8 @@ public class NumberOfWaysToScore {
   public static void main(String[] args) {
     countNumberOfWaysToReachScoreN(13,"");
     System.out.println(totalCount);
+    System.out.println("###########");
+    System.out.println(countNumberOfWaysToReachScoreNDP(13));
   }
 
 }
