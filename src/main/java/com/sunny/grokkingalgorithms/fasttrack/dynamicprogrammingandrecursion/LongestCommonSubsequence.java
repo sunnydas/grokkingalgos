726483a1af3f584a9456cd99b8d824e5d4d789d7
Sunny Das <das.sunny@gmail.com>
2018-06-14 21:30:58 +0530

    Find LCS using recursion O(2.pow(n))

--- src/main/java/com/sunny/grokkingalgorithms/fasttrack/dynamicprogrammingandrecursion/LongestCommonSubsequence.java
+++ src/main/java/com/sunny/grokkingalgorithms/fasttrack/dynamicprogrammingandrecursion/LongestCommonSubsequence.java
@@ -19,6 +19,28 @@ public class LongestCommonSubsequence {
    * @param s2
    * @return
    */
+  public static int findLCSRecursive(String s1,
+                                     String s2){
+    if(s1.length() == 0
+        || s2.length() == 0){
+      return 0;
+    }
+    if(s1.charAt(0)
+        == s2.charAt(0)){
+      return findLCSRecursive(s1.substring(1),s2.substring(1)) + 1;
+    }
+    else{
+      return Math.max(findLCSRecursive(s1,s2.substring(1)),
+          findLCSRecursive(s1.substring(1),s2));
+    }
+  }
+
+  /**
+   *
+   * @param s1
+   * @param s2
+   * @return
+   */
     public static int findLongestCommonSubsequence(String s1,
                                                    String s2){
       int maxCount = -1;
@@ -46,13 +68,13 @@ public class LongestCommonSubsequence {
      for(int i = s1Begin; i < s1.length() ; i++){
        for(int j = s2Begin; j < s2.length() ; j++){
          if(s1.charAt(i) == s2.charAt(j)){
-           System.out.print(s1.charAt(i) + " ");
+           //System.out.print(s1.charAt(i) + " ");
            sizeOfSubsequence++;
            s2Begin = j + 1;
          }
        }
      }
-     System.out.println();
+     //System.out.println();
      return sizeOfSubsequence;
    }
 
@@ -65,12 +87,14 @@ public class LongestCommonSubsequence {
     String s2 = "AEDFHR";
     //LCS = ADH
     System.out.println(findLongestCommonSubsequence(s1,s2));
+    System.out.println(findLCSRecursive(s1,s2));
     /*
     “AGGTAB” and “GXTXAYB”
      */
     s1 = "AGGTAB";
     s2 = "GXTXAYB";
     System.out.println(findLongestCommonSubsequence(s1,s2));
+    System.out.println(findLCSRecursive(s1,s2));
   }
 
 }
