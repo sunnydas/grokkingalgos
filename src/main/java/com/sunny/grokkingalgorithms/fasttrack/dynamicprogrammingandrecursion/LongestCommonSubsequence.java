a4a94f302486a7c5cd01efba6a093e10aa925d34
Sunny Das <das.sunny@gmail.com>
2018-06-14 21:19:34 +0530

    Find longest common subsequence O(n.pow(3)) solution.

---
+++ src/main/java/com/sunny/grokkingalgorithms/fasttrack/dynamicprogrammingandrecursion/LongestCommonSubsequence.java
@@ -0,0 +1,76 @@
+package com.sunny.grokkingalgorithms.fasttrack.dynamicprogrammingandrecursion;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+/**
+ * Created by sundas on 6/14/2018.
+ */
+public class LongestCommonSubsequence {
+
+  /*
+  Given two strings find count of the longest common subsequence
+   */
+
+  /**
+   *
+   * @param s1
+   * @param s2
+   * @return
+   */
+    public static int findLongestCommonSubsequence(String s1,
+                                                   String s2){
+      int maxCount = -1;
+      int j = 0;
+      for(int i = 0 ;i < s1.length() ; i++){
+        int size = findSizeOfCommonSubsequence(s1,s2,i);
+        if(size > maxCount){
+          maxCount = size;
+        }
+      }
+      return maxCount;
+    }
+
+  /**
+   *
+   * @param s1
+   * @param s2
+   * @param s1Begin
+   * @return
+   */
+   public static int findSizeOfCommonSubsequence(String s1,String s2,
+                                                 int s1Begin){
+     int sizeOfSubsequence = 0;
+     int s2Begin = 0;
+     for(int i = s1Begin; i < s1.length() ; i++){
+       for(int j = s2Begin; j < s2.length() ; j++){
+         if(s1.charAt(i) == s2.charAt(j)){
+           System.out.print(s1.charAt(i) + " ");
+           sizeOfSubsequence++;
+           s2Begin = j + 1;
+         }
+       }
+     }
+     System.out.println();
+     return sizeOfSubsequence;
+   }
+
+  /**
+   *
+   * @param args
+   */
+  public static void main(String[] args) {
+    String s1 = "ABCDGH";
+    String s2 = "AEDFHR";
+    //LCS = ADH
+    System.out.println(findLongestCommonSubsequence(s1,s2));
+    /*
+    “AGGTAB” and “GXTXAYB”
+     */
+    s1 = "AGGTAB";
+    s2 = "GXTXAYB";
+    System.out.println(findLongestCommonSubsequence(s1,s2));
+  }
+
+}
