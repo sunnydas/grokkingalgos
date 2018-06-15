230494451f3c44b3f9fccbd9a9b24f2dc0508200
Sunny Das <das.sunny@gmail.com>
2018-06-15 21:11:58 +0530

    Longest palindromic sequence dp solution o(n.squared).

--- src/main/java/com/sunny/grokkingalgorithms/fasttrack/dynamicprogrammingandrecursion/LongestPalindromicSubsequence.java
+++ src/main/java/com/sunny/grokkingalgorithms/fasttrack/dynamicprogrammingandrecursion/LongestPalindromicSubsequence.java
@@ -16,6 +16,42 @@ public class LongestPalindromicSubsequence {
    * @param s
    * @return
    */
+  public static int longestPalindromicSubsequenceDP(String s){
+   int[][] dp = new int[s.length()][s.length()];
+   for(int i = 0 ; i < dp.length ; i++){
+     dp[i][i] = 1;
+   }
+   for(int l = 2 ; l <= s.length() ; l++){
+     for(int i = 0 ; i < s.length() - l + 1; i++){
+       int j = i + l - 1;
+       if(l == 2
+           && s.charAt(i) == s.charAt(j)){
+         dp[i][j] = 2;
+       }
+       else if(s.charAt(i)
+           == s.charAt(j)){
+         dp[i][j] = 2 + dp[i + 1][j - 1];
+       }
+       else{
+         dp[i][j] = Math.max(dp[i][j - 1],
+             dp[i + 1][j]);
+       }
+     }
+   }
+   for(int i = 0 ; i < dp.length ; i++){
+     for(int j = 0 ; j < dp[i].length ; j++){
+       System.out.print(dp[i][j] + " ");
+     }
+     System.out.println();
+   }
+   return dp[0][dp[0].length - 1];
+  }
+
+  /**
+   *
+   * @param s
+   * @return
+   */
   public static int longestPalindromicSubsequence(String s){
     if(s == null
         || s.length() <= 0){
@@ -40,6 +76,10 @@ public class LongestPalindromicSubsequence {
   public static void main(String[] args) {
      String s = "BBABCBCAB";
     System.out.println(longestPalindromicSubsequence(s));
+    System.out.println(longestPalindromicSubsequenceDP(s));
+    String str = "agbdba";
+    System.out.println(longestPalindromicSubsequence(str));
+    System.out.println(longestPalindromicSubsequenceDP(str));
   }
 
 }
