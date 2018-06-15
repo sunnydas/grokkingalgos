ca5ac4799212776d3fb71469e0d8b3e38b63a48d
Sunny Das <das.sunny@gmail.com>
2018-06-15 22:53:55 +0530

    Eggs drop problem using memoization.Finally can calculate n = 100 , x = 2.

--- src/main/java/com/sunny/grokkingalgorithms/fasttrack/dynamicprogrammingandrecursion/EggsDroppingPuzzle.java
+++ src/main/java/com/sunny/grokkingalgorithms/fasttrack/dynamicprogrammingandrecursion/EggsDroppingPuzzle.java
@@ -5,6 +5,36 @@ package com.sunny.grokkingalgorithms.fasttrack.dynamicprogrammingandrecursion;
  */
 public class EggsDroppingPuzzle {
 
+  static int[][] eggsDropCache;
+
+  /**
+   *
+   * @param n
+   * @param x
+   * @return
+   */
+  public static int getMinNumberOfDroppingsMemoized(int n,int x){
+    //System.out.println(n + x);
+    if(eggsDropCache[n][x] >= 0){
+      return eggsDropCache[n][x];
+    }
+    if(n == 0 || n == 1 || x == 1){
+      eggsDropCache[n][x] = n;
+      return eggsDropCache[n][x];
+    }
+
+    int min = 1000;
+    for(int p = 1; p <= n; p++){
+      int temp = 1 + Math.max(getMinNumberOfDroppingsMemoized(p - 1,x - 1),
+          getMinNumberOfDroppingsMemoized(n - p,x));
+      if(temp < min){
+        min = temp;
+      }
+    }
+    eggsDropCache[n][x] = min;
+    return min;
+  }
+
   /*
   We are given n floors and x eggs , find minimum number of egg droppings
   to determine the floor from which a egg will break
@@ -60,8 +90,15 @@ public class EggsDroppingPuzzle {
    * @param args
    */
   public static void main(String[] args) {
-    int n = 10;
-    int x = 3;
-    System.out.println(getMinNumberOfDroppings(n,x));
+    int n = 100;
+    int x = 2;
+    //System.out.println(getMinNumberOfDroppings(n,x));
+    eggsDropCache = new int[n+1][x+1];
+    for(int i = 0 ; i < eggsDropCache.length ; i++){
+      for(int j = 0 ; j < eggsDropCache[i].length ; j++){
+        eggsDropCache[i][j] = -1;
+      }
+    }
+    System.out.println(getMinNumberOfDroppingsMemoized(n,x));
   }
 }
