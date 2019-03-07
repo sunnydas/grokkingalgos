cbb39835c567841d1f2f290a5436f968e402a172
Sunny Das <das.sunny@gmail.com>
2019-03-07 23:58:19 +0530

    Adding memorzatio to sum string problem.

--- src/main/java/com/sunny/grokkingalgorithms/boot2019/complexityanalysis/StrimgSumProblem.java
+++ src/main/java/com/sunny/grokkingalgorithms/boot2019/complexityanalysis/StrimgSumProblem.java
@@ -13,6 +13,24 @@ if n = 3, then the following strings will sum up to n:
      */
 
 
+    public static int findSumMemoization(int n, String s, int[] computed){
+        if(n == 0){
+            System.out.println(s);
+            return 1;
+        }
+        if(n < 1){
+            return 0;
+        }
+        if(computed[n] != -1){
+            return computed[n];
+        }
+        int sum = 0;
+        sum += findSumMemoization(n - 1,s,computed) + findSumMemoization(n - 2,s,computed)
+          + findSumMemoization(n - 3,s,computed);
+        computed[n] = sum;
+        return computed[n];
+    }
+
     public static void printStringSum(int n,String s){
         if(n == 0){
             System.out.println(s);
@@ -30,6 +48,13 @@ if n = 3, then the following strings will sum up to n:
     public static void main(String[] args) {
         System.out.println();
         printStringSum(3,"");
+        System.out.println("with memoization:");
+        int[] computed = new int[51];
+        for(int i = 0 ; i < computed.length ; i++){
+            computed[i] = -1;
+        }
+        System.out.println(findSumMemoization(50,"",computed));
+        System.out.println();
         System.out.println();
         printStringSum(2,"");
         System.out.println();
