4e517886d4dc26d95868a370479861df63fa4182
Sunny Das <das.sunny@gmail.com>
2019-10-12 18:21:31 +0530

    Is pre order a bst traversal using single stack.

--- src/main/java/com/sunny/grokkingalgorithms/boot2019/twize/CheckIfInputCanBePreorderTraversalForBST.java
+++ src/main/java/com/sunny/grokkingalgorithms/boot2019/twize/CheckIfInputCanBePreorderTraversalForBST.java
@@ -1,5 +1,7 @@
 package com.sunny.grokkingalgorithms.boot2019.twize;
 
+import java.util.Stack;
+
 public class CheckIfInputCanBePreorderTraversalForBST {
 
     /*
@@ -34,6 +36,33 @@ Output: false
 Given array cannot represent preorder traversal
 of a Binary Search Tree.
      */
+
+    public static boolean isPreOrderBSTUsingStacks(int[] input){
+        Stack<Integer> stack1 = new Stack<>();
+        int lastInOrderTop = Integer.MIN_VALUE;
+        boolean isPreOrderBST = true;
+        for(int i = 0 ; i < input.length ; i++){
+         //This is crucial
+        if (lastInOrderTop > input[i]) {
+            isPreOrderBST = false;
+            return isPreOrderBST;
+        }
+            if(stack1.isEmpty() || stack1.peek() > input[i]){
+                stack1.push(input[i]);
+            } else{
+                while (!stack1.isEmpty() && stack1.peek() < input[i]) {
+                    int elem = stack1.pop();
+                    if (elem > lastInOrderTop) {
+                        lastInOrderTop = elem;
+                    }
+                }
+                stack1.push(input[i]);
+            }
+        }
+        //System.out.println(lastInOrderTop);
+        //System.out.println(stack1);
+        return isPreOrderBST;
+    }
 
     public static boolean isPreorderForBST(int[] input,int index){
         if(index >= input.length){
@@ -89,6 +118,29 @@ of a Binary Search Tree.
         System.out.println(isPreorderForBST(input,0));
         input = new int[]{30, 20, 10, 40, 50};
         System.out.println(isPreorderForBST(input,0));
+        System.out.println();
+        System.out.println();
+        input = new int[]{2,4,3};
+        System.out.println(isPreOrderBSTUsingStacks(input));
+        input = new int[]{2, 4, 1};
+        System.out.println(isPreOrderBSTUsingStacks(input));
+        input = new int[]{40, 30, 35, 80, 100};
+        System.out.println(isPreOrderBSTUsingStacks(input));
+        input = new int[]{40, 30, 35, 20, 80, 100};
+        System.out.println(isPreOrderBSTUsingStacks(input));
+        input = new int[] {40, 30, 35, 20, 80, 100};
+        System.out.println(isPreOrderBSTUsingStacks(input));
+        input = new int[]{45, 25, 15, 35, 75};
+        System.out.println(isPreOrderBSTUsingStacks(input));
+        input = new int[]{50, 39, 44, 28, 85};
+        System.out.println(isPreOrderBSTUsingStacks(input));
+        input = new int[]{10, 25, 5};
+        System.out.println(isPreOrderBSTUsingStacks(input));
+        input = new int[]{30, 20, 10, 40, 50};
+        System.out.println(isPreOrderBSTUsingStacks(input));
+
+        input = new int[]{2, 4, 1};
+        System.out.println(isPreOrderBSTUsingStacks(input));
     }
 
 }
