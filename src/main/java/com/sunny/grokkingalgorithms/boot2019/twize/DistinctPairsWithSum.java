4a969f13452867baaa4b743caddc044da50737c8
Sunny Das <das.sunny@gmail.com>
2019-10-19 01:50:16 +0530

    Checking more code for earlier problems.

--- src/main/java/com/sunny/grokkingalgorithms/boot2019/twize/DistinctPairsWithSum.java
+++ src/main/java/com/sunny/grokkingalgorithms/boot2019/twize/DistinctPairsWithSum.java
@@ -22,8 +22,16 @@ class Pair{
     }
 
     @Override
+    public String toString() {
+        return "Pair{" +
+                "i=" + i +
+                ", j=" + j +
+                '}';
+    }
+
+    @Override
     public int hashCode() {
-        return Objects.hash(i, j);
+        return (i + j);
     }
 
     public void setI(int i) {
@@ -72,13 +80,40 @@ Output :  9
     public static void countDistinctPairsWithSum(int[] input,int k){
         Set<Pair> pairs = new HashSet<>();
         for(int i = 0 ; i < input.length ; i++){
-            for(int j = i + 1; j < input.length ; j++ ){
-                if(k - input[i] == input[j]) {
-                    pairs.add(new Pair(i,j));
+            for(int j = 0; j < input.length ; j++ ){
+                if(i != j) {
+                    if (k - input[i] == input[j]) {
+                        pairs.add(new Pair(input[i], input[j]));
+                    }
                 }
             }
         }
+        System.out.println(pairs);
         System.out.println(pairs.size());
+        int count = 0;
+    }
+
+
+    public static int countPairs(List<Integer> arr, long k) {
+        // Write your code here
+        Map<Integer,Integer> numFreqMap = new HashMap<>();
+        for(Integer num : arr){
+            if(numFreqMap.containsKey(num)){
+                numFreqMap.put(num,numFreqMap.get(num) + 1);
+            } else{
+                numFreqMap.put(num,1);
+            }
+        }
+        int count = 0;
+        for(Integer num : arr){
+            if(numFreqMap.containsKey(k - num)){
+                count += numFreqMap.get(k - num);
+                if(num == (k - num)){
+                    count--;
+                }
+            }
+        }
+        return (count/2);
     }
 
     public static void countPairs(int[] input,int k){
@@ -125,6 +160,22 @@ Output :  9
                 5, 4, 2, 1, 1, 1};
         countPairs(input,11);
         System.out.println();
+        input = new int[]{7,6,6,3,9,3,5,1,12};
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(7);
+        numbers.add(6);
+        numbers.add(6);
+        numbers.add(3);
+        numbers.add(9);
+        numbers.add(3);
+        numbers.add(5);
+        numbers.add(1);
+        numbers.add(12);
+        System.out.println(countPairs(numbers,47));
+        int[] ints = new int[]{5,7,9,13,11,6,6,3,3};
+        countDistinctPairsWithSum(ints,12);
+        ints = new int[]{1,3,46,1,3,9};
+        countDistinctPairsWithSum(ints,47);
     }
 
 }
--- src/main/java/com/sunny/grokkingalgorithms/boot2019/twize/ParseDomainnameFromStrings.java
+++ src/main/java/com/sunny/grokkingalgorithms/boot2019/twize/ParseDomainnameFromStrings.java
@@ -3,10 +3,39 @@ package com.sunny.grokkingalgorithms.boot2019.twize;
 import com.sun.org.apache.xerces.internal.impl.xpath.regex.Match;
 
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ParseDomainnameFromStrings {
+
+
+    public static String getPotentialDomainNames(List<String> lines){
+        List<String> domainNames = new ArrayList<>();
+        String regex = "(http://|https://)(.*)(/.*)";
+        Pattern pattern = Pattern.compile(regex);
+        for(String line: lines){
+            Matcher matcher = pattern.matcher(line);
+            while(matcher.find()){
+                domainNames.add(matcher.group(2));
+            }
+        }
+        Collections.sort(domainNames);
+        return getDomainNames(domainNames);
+    }
+
+    public static String getDomainNames(List<String> domainNames){
+        StringBuilder stringBuilder = new StringBuilder();
+        for(int i = 0 ; i < domainNames.size() ; i++){
+            stringBuilder.append(domainNames.get(i));
+            if(i != (domainNames.size() - 1)) {
+                stringBuilder.append(";");
+            }
+        }
+        return stringBuilder.toString();
+    }
 
     public static void main(String[] args) {
         String urlString = "https://www.example.domain.com/tester?query=5&query1=78";
@@ -18,6 +47,11 @@ public class ParseDomainnameFromStrings {
         while(matcher.find()){
             System.out.println(matcher.group(2));
         }
+        List<String> lines = new ArrayList<>();
+        lines.add("adaadasdsadqajdladjhttps://sunny.com/test");
+        lines.add("hello world");
+        lines.add("http://test.com/use");
+        System.out.println(getPotentialDomainNames(lines));
     }
 
 }
