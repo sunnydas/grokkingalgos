dda5d93d62d713bbfe2bec18e2756c82d14d63a8
Sunny Das <das.sunny@gmail.com>
2019-10-25 18:35:16 +0530

    Euclidean gcd iterative.

--- src/main/java/com/sunny/grokkingalgorithms/boot2019/collabs/GCDUsingEuclid.java
+++ src/main/java/com/sunny/grokkingalgorithms/boot2019/collabs/GCDUsingEuclid.java
@@ -2,6 +2,17 @@ package com.sunny.grokkingalgorithms.boot2019.collabs;
 
 public class GCDUsingEuclid {
 
+    public static int gcdIterative(int x,int y){
+        while(x != y){
+            if(x > y){
+                x = x - y;
+            } else{
+                y = y - x;
+            }
+        }
+        return x;
+    }
+
     public static int gcd(int x,int y){
         if(y == 0){
             return x;
@@ -11,6 +22,7 @@ public class GCDUsingEuclid {
 
     public static void main(String[] args) {
         System.out.println(gcd(36,60));
+        System.out.println(gcdIterative(36,60));
     }
 
 }
