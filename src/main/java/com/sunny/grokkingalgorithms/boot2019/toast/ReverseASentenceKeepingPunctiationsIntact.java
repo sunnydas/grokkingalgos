c1234aa3fffc00ecfd0f9d19baeb4210bf6b8b59
Sunny Das <das.sunny@gmail.com>
2019-10-29 21:56:02 +0530

    reverse sentences keeping punctuations intact.

--- src/main/java/com/sunny/grokkingalgorithms/boot2019/toast/ReverseASentenceKeepingPunctiationsIntact.java
+++ src/main/java/com/sunny/grokkingalgorithms/boot2019/toast/ReverseASentenceKeepingPunctiationsIntact.java
@@ -5,14 +5,24 @@ import java.util.regex.Pattern;
 public class ReverseASentenceKeepingPunctiationsIntact {
 
     public static String reverseKeepingPuncIntact(String sentence){
-        String[] splitted = sentence.split("[\\p{Punct}\\s]+");
-        StringBuilder sentenceBuilder = new StringBuilder();
-        int j  = 0;
-        for(int i = splitted.length - 1; i >= 0 ; i--){
-            sentenceBuilder.append(splitted[i]);
-            sentenceBuilder.append(" ");
-        }
-        return sentenceBuilder.toString();
+       char[] sentenceArr = sentence.toCharArray();
+       int i = 0;
+       int j = sentence.length() - 1;
+       while(i < j){
+           if(!Character.isAlphabetic(sentenceArr[i])){
+               i++;
+           }
+           else if(!Character.isAlphabetic(sentenceArr[j])){
+               j--;
+           }else{
+               char temp = sentenceArr[i];
+               sentenceArr[i] = sentenceArr[j];
+               sentenceArr[j] = temp;
+               i++;
+               j--;
+           }
+       }
+       return String.valueOf(sentenceArr);
     }
 
     public static String reverse(String sentence){
@@ -28,7 +38,7 @@ public class ReverseASentenceKeepingPunctiationsIntact {
     public static void main(String[] args) {
         String str = "this new world ";
         System.out.println(reverse(str));
-        str = "!Hello,world!";
+        str = "!Hello,                 world!                          ";
         System.out.println(reverseKeepingPuncIntact(str));
     }
 
