dd64e502eddca55d8f9f86a6309b832fd8e27192
Sunny Das <das.sunny@gmail.com>
2019-10-29 20:52:12 +0530

    Splitting on punctuatiosn.

--- src/main/java/com/sunny/grokkingalgorithms/boot2019/toast/ReverseASentenceKeepingPunctiationsIntact.java
+++ src/main/java/com/sunny/grokkingalgorithms/boot2019/toast/ReverseASentenceKeepingPunctiationsIntact.java
@@ -1,6 +1,19 @@
 package com.sunny.grokkingalgorithms.boot2019.toast;
 
+import java.util.regex.Pattern;
+
 public class ReverseASentenceKeepingPunctiationsIntact {
+
+    public static String reverseKeepingPuncIntact(String sentence){
+        String[] splitted = sentence.split("[\\p{Punct}\\s]+");
+        StringBuilder sentenceBuilder = new StringBuilder();
+        int j  = 0;
+        for(int i = splitted.length - 1; i >= 0 ; i--){
+            sentenceBuilder.append(splitted[i]);
+            sentenceBuilder.append(" ");
+        }
+        return sentenceBuilder.toString();
+    }
 
     public static String reverse(String sentence){
         String[] splitted = sentence.split(" ");
@@ -15,6 +28,8 @@ public class ReverseASentenceKeepingPunctiationsIntact {
     public static void main(String[] args) {
         String str = "this new world ";
         System.out.println(reverse(str));
+        str = "!Hello,world!";
+        System.out.println(reverseKeepingPuncIntact(str));
     }
 
 }
