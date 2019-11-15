47b56cfdb5e323db416c91178f9aef41048b0902
Sunny Das <das.sunny@gmail.com>
2019-11-15 12:11:53 +0530

    Trying to optimize this buy sell problem.

--- src/main/java/com/sunny/grokkingalgorithms/boot2019/toast/BuySellOneTransaction5Seconds.java
+++ src/main/java/com/sunny/grokkingalgorithms/boot2019/toast/BuySellOneTransaction5Seconds.java
@@ -5,6 +5,26 @@ import java.util.List;
 
 public class BuySellOneTransaction5Seconds {
 
+    public static long maxProfitAlt(List<Long> prices){
+       long maxProfit = 0;
+       long min = prices.get(0);
+       int i = 0;
+       int j = i+5;
+       while(j < prices.size()){
+           long cur = prices.get(i);
+           if(cur < min){
+               min = cur;
+           }
+           long diff = prices.get(j) - min;
+           if(diff > 0){
+               maxProfit = Math.max(maxProfit,diff);
+           }
+           i++;
+           j++;
+       }
+       return maxProfit;
+    }
+
     public static long  maxProfit(List<Long> prices){
         long maxProfit = 0;
         for(int i = 0 ; i < prices.size() ; i++){
@@ -24,7 +44,9 @@ public class BuySellOneTransaction5Seconds {
         prices.add(4L);
         prices.add(5L);
         prices.add(6L);
+        prices.add(7L);
         System.out.println(maxProfit(prices));
+        System.out.println(maxProfitAlt(prices));
     }
 
 
