741f1149eb92782b727a69ee5a2be2ab1f7ddc1d
Sunny Das <das.sunny@gmail.com>
2019-12-06 23:04:30 +0530

    Find missing elements in an arrays.

--- src/main/java/com/sunny/grokkingalgorithms/boot2019/FindMissingElementInASortedArray.java
+++ src/main/java/com/sunny/grokkingalgorithms/boot2019/FindMissingElementInASortedArray.java
@@ -1,8 +1,24 @@
 package com.sunny.grokkingalgorithms.boot2019;
 
 public class FindMissingElementInASortedArray {
-    
 
+
+    public static int findMissingElementInSortedArray(int[] ar,int size){
+        int start = 0;
+        int end = ar.length - 1;
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            if(ar[mid] != mid){
+                if(mid == 0 || ar[mid - 1] == mid - 1){
+                    return mid;
+                }
+                end = mid - 1;
+            }else{
+                start = mid + 1;
+            }
+        }
+        throw new IllegalArgumentException("No missing number");
+    }
 
     public static int findMissingElementInSortedArray(int[] input){
         int total = ((input.length + 1)*(input.length + 2))/2;
@@ -16,6 +32,10 @@ public class FindMissingElementInASortedArray {
         //1,2,4
         int[] input = new int[]{1,2,3,4,5,6,8,9};
         System.out.println(findMissingElementInSortedArray(input));
+        input = new int[]{0,1,2,3,4,5,6,8,9};
+        System.out.println(findMissingElementInSortedArray(input,input.length));
+        input = new int[]{0,1,2,3,4,5,6,7,8,10};
+        System.out.println(findMissingElementInSortedArray(input,input.length));
     }
 
 }
