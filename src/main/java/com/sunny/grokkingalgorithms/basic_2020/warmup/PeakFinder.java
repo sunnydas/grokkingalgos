31c55e4ae859d14412831c7674474c66068622b1
Sunny <das.sunny@gmail.com>
2020-05-23 19:01:35 +0800

    Peak finding simple warmup.

---
+++ src/main/java/com/sunny/grokkingalgorithms/basic_2020/warmup/PeakFinder.java
@@ -0,0 +1,37 @@
+package com.sunny.grokkingalgorithms.basic_2020.warmup;
+
+public class PeakFinder {
+
+    /*
+    Given a single domensional array , find a peak
+
+    [1,2,1,4,5]
+
+    2 is a peak
+
+    a<= b >= c
+     */
+
+
+    public static int findPeak(int[] input){
+        int index = -1;
+        for(int i = 1 ; i < input.length - 1 ; i++){
+            if((input[i] >= input[i - 1])
+                && (input[i] >=  input[i + 1])){
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
+
+    public static void main(String[] args) {
+        int[] input = new int[]{1,2,1,4,5};
+        System.out.println(input[findPeak(input)]);
+        input = new int[]{1,2,3,4,2};
+        System.out.println(input[findPeak(input)]);
+    }
+
+
+
+}
