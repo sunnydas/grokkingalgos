68428e184a1b62a3c7959c69a92217e02bb21468
Sunny Das <sunny.das@nucleussoftware.com>
2020-07-26 16:45:30 +0800

    Find pairs.

---
+++ src/main/java/com/sunny/grokkingalgorithms/basic_2020/warmup/Pairs.java
@@ -0,0 +1,66 @@
+package com.sunny.grokkingalgorithms.basic_2020.warmup;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Pairs {
+
+	/*
+	 * You will be given an array of integers and a target value. Determine the number of pairs of array elements that have a difference equal to a target value.
+
+For example, given an array of [1, 2, 3, 4] and a target value of 1, we have three values meeting the condition: , , and .
+
+Function Description
+
+Complete the pairs function below. It must return an integer representing the number of element pairs having the required difference.
+
+pairs has the following parameter(s):
+
+k: an integer, the target difference
+arr: an array of integers
+Input Format
+
+The first line contains two space-separated integers  and , the size of  and the target value.
+The second line contains  space-separated integers of the array .
+
+Constraints
+
+each integer  will be unique
+Output Format
+
+An integer representing the number of pairs of integers whose difference is .
+
+Sample Input
+
+5 2  
+1 5 3 4 2  
+Sample Output
+
+3
+Explanation
+
+There are 3 pairs of integers in the set with a difference of 2: [5,3], [4,2] and [3,1] .
+	 */
+	
+	// Complete the pairs function below.
+    static int pairs(int k, int[] arr) {
+    	int pairCount = 0;
+    	Map<Integer,Integer> trackerMap = new HashMap<Integer, Integer>();
+    	for(int i = 0 ; i < arr.length ; i++) {
+    		trackerMap.put(arr[i],i);
+    	}
+    	for(int i = 0; i < arr.length ; i++) {
+    		if(trackerMap.containsKey(k + arr[i]) && 
+    				trackerMap.get(k + arr[i]) != i) {
+    			pairCount++;    			
+    		}
+    	}
+    	return pairCount;
+    }
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
