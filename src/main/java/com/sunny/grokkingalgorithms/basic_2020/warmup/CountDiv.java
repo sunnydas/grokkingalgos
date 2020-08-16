a4929df042f5760658fe382087552ed3b47b9729
Sunny Das <das.sunny@gmail.com>
2020-08-17 00:34:34 +0800

    Count div optimized for O(1).

--- src/main/java/com/sunny/grokkingalgorithms/basic_2020/warmup/CountDiv.java
+++ src/main/java/com/sunny/grokkingalgorithms/basic_2020/warmup/CountDiv.java
@@ -21,8 +21,60 @@ K is an integer within the range [1..2,000,000,000];
 A ≤ B.
 	 */
 	
+	
 	public static int solution(int A, int B, int K) {
+		/*
+		 * 6, 11 2
+		 * 11/2 - 6/2 + 1
+		 * 5 - 3 + 1
+		 * The number of multiples of z in a number n is simply n / z
+
+/ being the integer division, meaning decimals that could result from the division are simply ignored (for instance 17/5 => 3 and not 3.4).
+
+Now, in a range from x to y, how many multiples of z are there?
+
+Let see how many multiples m we have up to y
+
+0----------------------------------x------------------------y
+-m---m---m---m---m---m---m---m---m---m---m---m---m---m---m---
+You see where I'm going... to get the number of multiples in the range [ x, y ], get the number of multiples of y then subtract the number of multiples before x, (x-1) / z
+
+Solution: ( y / z ) - (( x - 1 ) / z )
+
+Programmatically, you could make a function numberOfMultiples
+
+function numberOfMultiples(n, z) {
+   return n / z;
+}
+to get the number of multiples in a range [x, y]
+
+numberOfMultiples(y) - numberOfMultiples(x-1)
+The function is O(1), there is no need of a loop to get the number of multiples.
+
+Examples of results you should find
+
+[30, 90] ÷ 13 => 4
+[1, 1000] ÷ 6 => 166
+[100, 1000000] ÷ 7 => 142843
+[777, 777777777] ÷ 7 => 111111001
+For the first example, 90 / 13 = 6, (30-1) / 13 = 2, and 6-2 = 4
+
+---26---39---52---65---78---91--
+     ^                      ^
+     30<---(4 multiples)-->90
+		 */
+		if(A % K == 0) {
+			return (B/K) - (A/K) + 1;			
+		}
+		return (B/K) - (A/K);
+	}
+	
+	public static int solutionAlt(int A, int B, int K) {
 		int count = 0;
+		/*
+		 * 5 6 7 8
+		 * 
+		 */
 		for(int i = A; i <= B ; i++) {
 			if(i % K  == 0) {
 				count++;				
@@ -30,9 +82,11 @@ A ≤ B.
 		}
 		return count;
 	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(solution(6, 11, 2));
+		System.out.println(solution(5, 8, 7));
 	}
 
 }
