bfa8a22b81e7f8e74c63068631e4b009e1aa2746
Sunny Das <das.sunny@gmail.com>
2020-08-18 19:18:51 +0800

    Brute force solution quadratic complexity plus extra space.

--- src/main/java/com/sunny/grokkingalgorithms/basic_2020/warmup/MinAvgTwoSlice.java
+++ src/main/java/com/sunny/grokkingalgorithms/basic_2020/warmup/MinAvgTwoSlice.java
@@ -1,5 +1,13 @@
 package com.sunny.grokkingalgorithms.basic_2020.warmup;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public class MinAvgTwoSlice {
 	
 	/*
@@ -61,46 +69,57 @@ slice (1, 2), whose average is (2 + 2) / 2 = 2;
 slice (3, 4), whose average is (5 + 1) / 2 = 3;
 slice (1, 4), whose average is (2 + 2 + 5 + 1) / 4 = 2.5.
 
-3)8(2.6
-  6
-  -- 
- 2  
- 
-     avg = (a + b + c)/3
-     
-     avg = (a[i] + P[i - 1])/n
-                                   
-     
-     4 2 2 5
-     
-     4 6 8 13      
-     
-     4 3 2.6 3.25
+1 2 3 
 
-     4 -1 -.4 .65  
+1 3 6
+
+1 3/2 6/3
+
+1 1.5 3 
 		 */
-		double minAvg = Double.MAX_VALUE;
-		int minIndex = 0;
-		double[] prefixSums = new double[A.length];
-		prefixSums[0] = A[0];
+		//List<Integer> minIndexes = new ArrayList<Integer>();
+		double[] prefixSum = new double[A.length];
+		prefixSum[0] = A[0];
+		//minIndexes.add(0);
 		for(int i = 1; i < A.length ; i++) {
-			prefixSums[i] += A[i - 1];
+			prefixSum[i] = A[i] + prefixSum[i - 1];			
 		}
-		//print(prefixSums);
-		for(int i = 0 ; i < prefixSums.length ; i++) {
-		    for(int j = i+1 ; j < prefixSums.length ; j++) {
-		    	double curAverage = (prefixSums[j] - prefixSums[i])/(j - i + 1);
-		    	if(curAverage < minAvg) {
-		    		//System.out.println(curAverage);
-		    		//System.out.println(i);
-		    		//System.out.println(j);
-		    		//System.out.println();
-		    		minAvg = curAverage;
-		    		minIndex = i;
-		    	}
-		    }
+		//print(prefixSum);
+		/*
+		 * 4 2 2 1
+		 * 4 6 8 9
+		 */
+		double min = Double.MAX_VALUE;
+		Map<Double,List<Integer>> tracker = new HashMap<Double,List<Integer>>();
+		for(int i = 0 ; i < prefixSum.length ;i++) {
+			for(int j = i+1; j < prefixSum.length ; j++) {
+				double avg;
+				if(i == 0) {
+					//System.out.println(prefixSum[j]);
+					//System.out.println(j);
+					avg = (prefixSum[j])/(j+1);
+					//System.out.println(avg);
+				}else {
+					avg = (prefixSum[j] - prefixSum[i - 1])/(j - i + 1);
+				}
+				//System.out.println(i);
+				//System.out.println(j);
+				//System.out.println(avg);
+				if(avg < min) {
+					min = avg;
+					if(tracker.containsKey(min)) {
+						tracker.get(min).add(i);							
+					}else {
+						List<Integer> indexes = new ArrayList<Integer>();
+						indexes.add(i);
+						tracker.put(min, indexes);
+					}
+				}
+			}
 		}
-		return A[minIndex];
+		//System.out.println(min);
+		//System.out.println(tracker);
+		return tracker.get(min).get(0);
 	}
 	
 	public static int findMin(double[] input) {
@@ -113,6 +132,13 @@ slice (1, 4), whose average is (2 + 2 + 5 + 1) / 4 = 2.5.
 			}
 		}
 		return minIndex;
+	}
+	
+	public static void print(int[] arr) {
+		for(int i = 0; i < arr.length ; i++) {
+			System.out.print(arr[i] + " ");			
+		}
+		System.out.println();
 	}
 	
 	public static void print(double[] arr) {
@@ -135,8 +161,12 @@ slice (1, 4), whose average is (2 + 2 + 5 + 1) / 4 = 2.5.
 		 */
 		int[] input = new int[] {4,2,2,5,1,5,8};
 		System.out.println(solution(input));
-		input = new int[]{1,2,3,4};
+		input = new int[]{1,2,1,1};
 		System.out.println(solution(input));
+		double a = 3.0;
+		int b = 2;
+		double c = a/b;
+		//System.out.println(c);
 	}
 
 }
