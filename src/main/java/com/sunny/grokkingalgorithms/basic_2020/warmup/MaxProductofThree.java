61c949a8ca578daae92cf9c757c1153fae151f64
Sunny Das <das.sunny@gmail.com>
2020-08-20 20:09:06 +0800

    SLightly better solution using sorting.

--- src/main/java/com/sunny/grokkingalgorithms/basic_2020/warmup/MaxProductofThree.java
+++ src/main/java/com/sunny/grokkingalgorithms/basic_2020/warmup/MaxProductofThree.java
@@ -3,6 +3,23 @@ package com.sunny.grokkingalgorithms.basic_2020.warmup;
 import java.util.Arrays;
 import java.util.Comparator;
 
+class Elem implements Comparable<Elem>{
+	
+	public int index;
+	public int value;
+	
+	@Override
+	public int compareTo(Elem o) {
+		if(this.value > o.value && this.index > o.index) {
+			return 1;
+		}else if(this.value < o.value) {
+			return -1;			
+		} 
+		return 0;
+	}
+	
+	
+}
 public class MaxProductofThree {
 	
 	/*
@@ -45,7 +62,42 @@ N is an integer within the range [3..100,000];
 each element of array A is an integer within the range [−1,000..1,000].
 	 */
 	
-	 public static int solution(int[] A) {
+	
+	public static int solution(int[] A) {
+		int max = Integer.MIN_VALUE;
+		Arrays.parallelSort(A);
+		int i = A.length - 1;
+		max = Math.max(A[i]*A[i-1]*A[i-2],A[i]*A[0]*A[1]);
+		return max;
+	}
+	
+	public static int solutionAlt1(int[] A) {
+		int max = Integer.MIN_VALUE;
+		Elem[] elems = new Elem[A.length];
+		for(int i = 0 ; i < A.length ; i++) {
+			Elem elem = new Elem();
+			elem.index = i;
+			elem.value = A[i];
+			elems[i] = elem;
+		}
+		Arrays.sort(elems);
+		print(elems);
+		int i = 0;
+		int j = elems.length - 1;
+		int k = i + (j - i)/2;
+		return max;
+	}
+	
+	private static void print(Elem[] elems) {
+		System.out.println();
+		for(Elem elem  : elems) {
+			System.out.print(" " + elem.value + " " + elem.index + " , ");
+			//System.out.println();
+		}
+		System.out.println();
+	}
+	
+	 public static int solutionAlt(int[] A) {
 		 int max = Integer.MIN_VALUE;
 		 /*
 		  *  
@@ -53,6 +105,9 @@ each element of array A is an integer within the range [−1,000..1,000].
 		  *
 		  * -3 -2 1 2 5 6
 		  * 
+		  * -5 5 -5 4
+		  * 
+		  * -5 -5 5 4
 		  * 
 		  * 
 		  */
@@ -72,6 +127,8 @@ each element of array A is an integer within the range [−1,000..1,000].
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] input = new int[] {-3,1,2,-2,5,6};
+		System.out.println(solution(input));
+		input = new int[] { -5, 5, -5, 4};
 		System.out.println(solution(input));
 	}
 
