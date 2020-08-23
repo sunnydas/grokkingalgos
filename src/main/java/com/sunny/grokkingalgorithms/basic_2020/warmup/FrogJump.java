10ce005568c686db5d43071bd3184d5b5f9dd09f
Sunny Das <das.sunny@gmail.com>
2020-08-23 18:31:58 +0800

    Checkpoint commit. visit later

--- src/main/java/com/sunny/grokkingalgorithms/basic_2020/warmup/FrogJump.java
+++ src/main/java/com/sunny/grokkingalgorithms/basic_2020/warmup/FrogJump.java
@@ -1,5 +1,18 @@
 package com.sunny.grokkingalgorithms.basic_2020.warmup;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+class Jump{
+	public int pos;
+	public Jump(int pos, int mov) {
+		super();
+		this.pos = pos;
+		this.mov = mov;
+	}
+	public int mov;
+}
 public class FrogJump {
 
 	/*
@@ -58,49 +71,102 @@ N is an integer within the range [0..100,000];
 each element of array A is an integer that can have one of the following values: 0, 1.
 	 */
 	
-	public static int fib(int n) {
+	public static int fib(int n,int[] cache) {
+		if(cache[n] > 0) {
+			return cache[n];			
+		}
 		int fib = 0;
-		if(n == 0) {
-			return 0;
-		}
-		if(n == 1) {
-			return 1;			
-		}
-		int fibN2 = 0;
-		int fibN1 = 1;
+		cache[0] = 1;
+		cache[1] = 2;
+		int fibN2 = cache[0];
+		int fibN1 = cache[1];
+		cache[1] = 1;
 		for(int i = 2; i <= n; i++) {
 			fib = fibN1 + fibN2;
 			fibN2 = fibN1;
 			fibN1 = fib;
+			cache[i] = fib;
 		}
-		return fib;
+		cache[n] = fib;
+		return cache[n];
+	}
+	
+	private static List < Integer > fibArray(int n) {
+	    List < Integer > fibs = new ArrayList < > ();
+	    fibs.add(1);
+	    fibs.add(2);
+	    while (fibs.get(fibs.size() - 1) + fibs.get(fibs.size() - 2) <= n) {
+	        fibs.add(fibs.get(fibs.size() - 1) + fibs.get(fibs.size() - 2));
+	    }
+	    return fibs;
 	}
 	
 	public static int solution(int[] A) {
-		
-		return 0;
+		int n = A.length;
+	    List < Integer > fibs = fibArray(n + 1);
+	    Queue < Jump > positions = new LinkedList < Jump > ();
+	    boolean[] visited = new boolean[n + 1];
+
+	    if (A.length <= 2)
+	        return 1;
+
+	    for (int i = 0; i < fibs.size(); i++) {
+	        int initPos = fibs.get(i) - 1;
+	        if (A[initPos] == 0 || visited[initPos])
+	            continue;
+	        positions.add(new Jump(initPos, 1));
+	        visited[initPos] = true;
+	    }
+
+	    while (!positions.isEmpty()) {
+	        Jump jump = positions.remove();
+	        for (int j = fibs.size() - 1; j >= 0; j--) {
+	            int nextPos = jump.pos + fibs.get(j);
+	            if (nextPos == n)
+	                return jump.mov + 1;
+	            else if (nextPos < n && A[nextPos] == 1 && !visited[nextPos]) {
+	                positions.add(new Jump(nextPos, jump.mov + 1));
+	                visited[nextPos] = true;
+	            }
+	        }
+	    }
+	    return -1;
 	}
 	
-	
+	public static void print(int[] cache) {
+		for(int i : cache) {
+			System.out.print(i + " ");
+		}
+		System.out.println();
+	}
+		
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         /*
          * A[0] = 0
     A[1] = 0
     A[2] = 0
-    A[3] = 1
-    A[4] = 1
+    A[3] = 4
+    A[4] = 5
     A[5] = 0
-    A[6] = 1
+    A[6] = 7
     A[7] = 0
     A[8] = 0
     A[9] = 0
     A[10] = 0
+    A[11] = 12
          */
-		int[] input = new int[] {0,0,0,1,1,0,1,0,0,0,0};
+		int[] input = new int[] {0,0,0,1,1,0,1,0,0,0};
+		/*
+		 * 0,0,0,4,5,0,7,0,0,0,0
+		 * 
+		 * 0,0,0,1,1
+		 */
 		System.out.println(solution(input));
-		System.out.println(fib(5));
-		System.out.println(fib(3));
+		int[] cache = new int[10];
+		//System.out.println(fib(5,cache));
+		//System.out.println(fib(3, cache));
+		//print(cache);
 	}
 
 }
