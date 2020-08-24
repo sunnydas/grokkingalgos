c721090713fbf03c0072af4f464a7084f63e1136
Sunny Das <das.sunny@gmail.com>
2020-08-24 17:19:32 +0800

    All permutations of string of any length. Brute force.

---
+++ src/main/java/com/sunny/grokkingalgorithms/basic_2020/warmup/Permutations.java
@@ -0,0 +1,49 @@
+package com.sunny.grokkingalgorithms.basic_2020.warmup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Permutations {
+	
+	public static List<String> permute(String s) {
+		if(s.length() <= 0) {
+			List<String> empty = new ArrayList<String>();
+			empty.add(" ");
+			return empty;
+		}
+	    char current = s.charAt(0);
+	    String remaining = s.substring(1);
+	    List<String> sub = permute(remaining);
+	    List<String> overall = new ArrayList<String>();
+	    for(String str : sub) {
+	    	//System.out.println(str);
+	    	for(int i =  0; i < str.length() ; i++) {
+	    		StringBuilder builder = new StringBuilder(str);
+	    		builder.insert(i, current);
+	    		overall.add(builder.toString());
+	    	}
+	    }
+	    
+	    overall.addAll(sub);
+	    return overall;
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        String s = "abc";
+        /*
+         * abc
+         * a  bc 
+         *      b  
+         *          c
+         *    
+         *                abcd
+         *                dbacd
+         *                bca
+         *             
+         */
+        System.out.println(permute(s));
+	}
+
+}
