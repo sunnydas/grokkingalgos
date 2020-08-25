15ecf313f17cddc569503b37041de98146890fbf
Sunny Das <das.sunny@gmail.com>
2020-08-25 14:29:08 +0800

    Longest increasing subsequence using DP.

--- src/main/java/com/sunny/grokkingalgorithms/basic_2020/warmup/LongestIncreasingSubsequence.java
+++ src/main/java/com/sunny/grokkingalgorithms/basic_2020/warmup/LongestIncreasingSubsequence.java
@@ -1,6 +1,30 @@
 package com.sunny.grokkingalgorithms.basic_2020.warmup;
 
+import java.util.Arrays;
+
 public class LongestIncreasingSubsequence {
+	
+	public static int longestIncreasingSubsequenceDP(int[] input) {
+		int[] tracker = new int[input.length];
+		Arrays.fill(tracker, 1);
+		for(int i = 1; i < tracker.length ; i++) {
+			for(int j = 0 ; j < i; j++) {
+				if(input[i] > input[j]) {
+					if(tracker[j] + 1 > tracker[i]) {
+						tracker[i] = tracker[j] + 1;
+					}
+				}
+			}
+		}
+		int max = tracker[0];
+	    System.out.println(Arrays.toString(tracker));
+		for(int i = 1; i < tracker.length ; i++) {
+			if(tracker[i] > max) {
+				max = tracker[i];				
+			}
+		}
+		return max;
+	}
 	
 	public static int longestIncreasingSubsequence(int[] input,
 			int currentPos,
@@ -27,6 +51,7 @@ The longest increasing subsequence is 3, 10, 20
 		 */
 		int[] input = {3, 10, 2, 1, 20};
 		System.out.println(longestIncreasingSubsequence(input, 0, Integer.MIN_VALUE));
+		System.out.println(longestIncreasingSubsequenceDP(input));
 	}
 
 }
