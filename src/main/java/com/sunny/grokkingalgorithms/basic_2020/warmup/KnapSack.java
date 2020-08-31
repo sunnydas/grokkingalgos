df5b91de3b2414addf9f3d6e0d0362816ba09191
Sunny Das <das.sunny@gmail.com>
2020-08-31 16:16:49 +0800

    DP based algorithm for knap sack.

--- src/main/java/com/sunny/grokkingalgorithms/basic_2020/warmup/KnapSack.java
+++ src/main/java/com/sunny/grokkingalgorithms/basic_2020/warmup/KnapSack.java
@@ -1,7 +1,64 @@
 package com.sunny.grokkingalgorithms.basic_2020.warmup;
 
+import java.util.Arrays;
+
 public class KnapSack {
 
+		
+	public static int maxProfitDP(int[] weights,
+			int[] profits,
+			int k) {
+		int[][] dp = new int[profits.length][k+1];
+		/*
+		 * Items: { Apple, Orange, Banana, Melon }
+Weights: { 2, 3, 1, 4 }
+Profits: { 4, 5, 3, 7 }
+Knapsack capacity: 5
+
+             0 1 2 3 4 5  
+           0 0 0 0 0 0 0  
+           1 0 
+           2 0
+           //           3
+           
+           
+		 */
+		for(int j = 0; j < dp.length; j++) {
+			dp[j][0] = 0;			
+		}
+		
+		for(int j = 0; j < dp.length; j++) {
+			if(weights[0] <= j) {
+				dp[0][j] = profits[0];
+			}
+		}
+		
+		for(int i = 1; i < dp.length ; i++) {
+			for(int j = 1 ; j < dp[i].length ; j++) {
+				int profit1 = 0;
+				int profit2 = 0;
+				if(weights[i] <= j) {
+					profit1 = dp[i - 1][j - weights[i]] + profits[i];
+				}
+				profit2 = dp[i - 1][j];
+				dp[i][j] = Math.max(profit1, profit2);
+			}
+		}
+		//System.out.println(Arrays.toString(dp));
+		print(dp);
+		return dp[dp.length - 1][k];		
+	}
+	
+	public static void print(int[][] dp) {
+		for(int i = 0 ; i < dp.length ; i++) {
+			for(int j = 0; j < dp[i].length ; j++) {
+				System.out.print(dp[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+	
+	
 	public static int maxProfitWithMemoization(int[] weights,
 			 int[] profits,
 			 int k,
@@ -83,6 +140,11 @@ Knapsack capacity: 5
         System.out.println(maxProfit(weights, profits, k,0,0,""));
         Integer[][] dp = new Integer[profits.length][k+1];
         System.out.println(maxProfitWithMemoization(weights, profits, k, 0,0,"",dp));
-	}
+        System.out.println(maxProfitDP(weights, profits, k));
+        profits = new int[]{1, 6, 10, 16};
+        weights = new int[]{1, 2, 3, 5}; 
+        System.out.println(maxProfitDP(weights, profits, 7));
+        System.out.println(maxProfitDP(weights, profits, 6));
+ 	}
 
 }
