41132e812641cf34877b6a19849ebcae0abc073b
Sunny Das <das.sunny@gmail.com>
2020-08-31 23:05:03 +0800

    Birthday cakes

---
+++ src/main/java/com/sunny/grokkingalgorithms/basic_2020/warmup/BirthdayCakeCandles.java
@@ -0,0 +1,91 @@
+package com.sunny.grokkingalgorithms.basic_2020.warmup;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+public class BirthdayCakeCandles {
+
+	/*
+	 * You are in charge of the cake for your niece's birthday and have decided the cake will have one candle for each year of her total age. When she blows out the candles, she’ll only be able to blow out the tallest ones. Your task is to find out how many candles she can successfully blow out.
+
+For example, if your niece is turning  years old, and the cake will have  candles of height , , , , she will be able to blow out  candles successfully, since the tallest candles are of height  and there are  such candles.
+
+Function Description
+
+Complete the function birthdayCakeCandles in the editor below. It must return an integer representing the number of candles she can blow out.
+
+birthdayCakeCandles has the following parameter(s):
+
+ar: an array of integers representing candle heights
+Input Format
+
+The first line contains a single integer, , denoting the number of candles on the cake.
+The second line contains  space-separated integers, where each integer  describes the height of candle .
+
+Constraints
+
+Output Format
+
+Return the number of candles that can be blown out on a new line.
+
+Sample Input 0
+
+4
+3 2 1 3
+Sample Output 0
+
+2
+Explanation 0
+
+We have one candle of height , one candle of height , and two candles of height . Your niece only blows out the tallest candles, meaning the candles where . Because there are  such candles, we print  on a new line.
+	 */
+	
+	private static final Scanner scanner = new Scanner(System.in);
+	
+	static int birthdayCakeCandles(int[] ar) {
+		Arrays.parallelSort(ar);
+		int count = 1;
+		for(int i = ar.length - 2; i >= 0; i--) {
+			if(ar[i] == ar[i + 1]) {
+				count++;				
+			}else {
+				break;
+			}
+		}
+		return count;
+    }
+
+    public static void main(String[] args) throws IOException {
+        //BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] ar = new int[arCount];
+
+        String[] arItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < arCount; i++) {
+            int arItem = Integer.parseInt(arItems[i]);
+            ar[i] = arItem;
+        }
+
+        int result = birthdayCakeCandles(ar);
+        System.out.println(result);
+
+        //bufferedWriter.write(String.valueOf(result));
+        //bufferedWriter.newLine();
+
+        //bufferedWriter.close();
+
+        scanner.close();
+    }
+
+}
