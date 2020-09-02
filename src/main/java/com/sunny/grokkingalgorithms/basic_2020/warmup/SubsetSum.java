57bbef9811d1f8bd8074c3fa41819fbce1d16deb
Sunny Das <das.sunny@gmail.com>
2020-09-02 20:48:51 +0800

    Subset sum using memoization.

--- src/main/java/com/sunny/grokkingalgorithms/basic_2020/warmup/SubsetSum.java
+++ src/main/java/com/sunny/grokkingalgorithms/basic_2020/warmup/SubsetSum.java
@@ -33,14 +33,50 @@ The given set does not have any subset whose sum is equal to '6'.
 		return subsetSum(input, k, index+1);
 	}
 	
+	public static boolean subSetSumCached(int[] input,
+			int k) {
+			Boolean[][] dp = new Boolean[input.length][k+1];
+			return subsetSumMemoization(input,
+					k, 
+					0, 
+					dp);
+	}
+	
+	public static boolean subsetSumMemoization(int[] input,
+			int k,
+			int index,
+			Boolean[][] dp) {
+		if(k == 0) {
+			return true;
+		}
+		if(input.length == 0 
+				|| index >= input.length) {
+			return false;
+		}
+		if(dp[index][k] == null) {
+		    if(input[index] <= k) {
+		    	boolean isSum = subsetSumMemoization(input, k - input[index], index+1, dp);
+		    	dp[index][k] = isSum;
+		    	if(isSum) {
+		    		return true;
+		    	}
+		    }
+		}
+		dp[index][k] = subsetSumMemoization(input, k, index+1,dp);
+		return dp[index][k];
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] input = {1, 2, 3, 7};
 		System.out.println(subsetSum(input,6,0));
+		System.out.println(subSetSumCached(input, 6));
 		int[] input1 = {1, 2, 7, 1, 5};
 		System.out.println(subsetSum(input1,10,0));
+		System.out.println(subSetSumCached(input1, 10));
 		int[] input2 = {1, 3, 4, 8};
 		System.out.println(subsetSum(input2,6,0));
+		System.out.println(subSetSumCached(input2, 6));
 	}
 
 }
