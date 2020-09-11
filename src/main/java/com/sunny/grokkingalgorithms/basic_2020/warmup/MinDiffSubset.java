bdb4611967fafe98eb3a0ae25a20df8966077ab5
Sunny Das <das.sunny@gmail.com>
2020-09-11 18:47:09 +0800

    Memoized version.

--- src/main/java/com/sunny/grokkingalgorithms/basic_2020/warmup/MinDiffSubset.java
+++ src/main/java/com/sunny/grokkingalgorithms/basic_2020/warmup/MinDiffSubset.java
@@ -25,11 +25,30 @@ Example 2: #
 		return Math.min(diff1,diff2);
 	}
 	
+	public static int minDiffMemoized(int[] input,
+			int index,
+			int sum1,
+			int sum2,
+			Integer[] dp) {
+		if(index >= input.length) {
+			return Math.abs(sum1 - sum2);			
+		}
+		if(dp[index] != null) {
+			return dp[index];
+		}
+		int diff1 = minDiff(input,index+1,sum1+input[index],sum2);
+		int diff2 = minDiff(input,index+1,sum1,sum2 + input[index]);
+		dp[index] = Math.min(diff1,diff2);
+		return dp[index];
+	}
+	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         int[] input = {1, 2, 3, 9};
         System.out.println(minDiff(input, 0, 0, 0));
+        Integer[] dp = new Integer[input.length];
+        System.out.println(minDiffMemoized(input, 0, 0, 0, dp));
 	}
 
 }
